84b23a5a38a8c6ebb1bafe9266c4469e43bb71f6
cycedmund <cycedmund@gmail.com>
2023-09-29 21:23:12 +0800

    feat: add Auth Page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,4 +1,7 @@
 import debug from "debug";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import AuthPage from "../AuthPage/AuthPage";
 
 const log = debug("nextfit:src:App");
 localStorage.debug = "nextfit:*";
@@ -6,7 +9,9 @@ localStorage.debug = "nextfit:*";
 log("Start React");
 
 const App = () => {
-  return <div>App</div>;
+  const [user, setUser] = useState(null);
+
+  return <main>{user ? <Routes></Routes> : <AuthPage />}</main>;
 };
 
 export default App;

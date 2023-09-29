a43044327688bde79e01bcbdaa24c6c8942fee04
cycedmund <cycedmund@gmail.com>
2023-09-29 21:28:45 +0800

    feat: add Home Page and setup Routes

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,6 +2,7 @@ import debug from "debug";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "../AuthPage/AuthPage";
+import HomePage from "../HomePage/HomePage";
 
 const log = debug("nextfit:src:App");
 localStorage.debug = "nextfit:*";
@@ -11,7 +12,17 @@ log("Start React");
 const App = () => {
   const [user, setUser] = useState(null);
 
-  return <main>{user ? <Routes></Routes> : <AuthPage />}</main>;
+  return (
+    <main>
+      {user ? (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
+  );
 };
 
 export default App;

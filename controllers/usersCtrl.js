c834bb77eba40a8d4b173e1fe043865e75db4ff6
cycedmund <cycedmund@gmail.com>
2023-10-04 01:12:59 +0800

    feat: set up login server-side

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,20 +1,35 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const debug = require("debug")("nextfit:controllers:usersCtrl");
 
 async function create(req, res) {
   try {
-    debug("created new user: %o", req.body);
     const newUser = await User.create(req.body);
+    debug("created new user: %o", req.body);
     const token = createJWT(newUser);
     res.status(201).json(token);
   } catch (err) {
-    debug("Error creating: %s", err);
+    debug("Error creating: %o", err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-//* ===== Helper Function ===== *//
+async function login(req, res) {
+  debug("login user body: %o", req.body);
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    res.status(200).json(createJWT(user));
+  } catch (err) {
+    debug("Error creating: %o", err);
+    res.status(401).json({ error: "Bad Credentials" });
+  }
+}
+
+//* ===== Helper Functions ===== *//
 
 function createJWT(user) {
   return jwt.sign(
@@ -25,4 +40,4 @@ function createJWT(user) {
   );
 }
 
-module.exports = { create };
+module.exports = { create, login };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersCtrl = require("../controllers/usersCtrl");
 
 router.post("/", usersCtrl.create);
+router.post("/login", usersCtrl.login);
 
 module.exports = router;

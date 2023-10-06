7da0b8c7e8edf2a0e23393aa38f4c3f9999e2dbd
Vakarian <fazmn7@gmail.com>
2023-10-06 17:22:47 +0800

    feat: react side of favourites page functionalities

--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -1,5 +1,23 @@
+import FavOutfitCard from "../../components/FavOutfitCard/FavOutfitCard";
+
+//* fetch from database
+const favOutfits = [
+  { outfitID: 17691, apparelID: [ 456, 123 ] },
+  { outfitID: 17691, apparelID: [ 789, 123 ] },
+];
+
 function FavouritesPage() {
-  return <div>FavouritesPage</div>;
+  return <div>
+    <div>
+      <header className="mx-4 font-inter font-thin text-2xl">Favourite Outfits</header>
+      <div className="grid grid-cols-5 gap-2 border-2 p-6 m-4">
+      {
+        favOutfits?.map(outfit => <FavOutfitCard key={outfit.outfitID} outfit={outfit}/>)
+      }
+      </div>
+    </div>
+    
+  </div>;
 }
 
 export default FavouritesPage;

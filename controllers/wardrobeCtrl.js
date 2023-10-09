3cc4933d8615f2cf29ada9ef54153b303c1bf444
ryu <cristellechoo@hotmail.com>
2023-10-09 17:27:20 +0800

    fix: change findOneAndUpdate to findOneByIdAndUpdate

--- controllers/wardrobeCtrl.js
+++ controllers/wardrobeCtrl.js
@@ -84,17 +84,22 @@ async function del(req, res) {
 
 async function updateOne(req, res) {
   debug("see req.user: %o", req.user);
+  debug("id", req.params.apparelId)
   try {
-    const updatedApparel = await Wardrobe.findOneAndUpdate(
-      { _id: req.params.apparelId , user: req.user._id },
-      { $set: { mainCategory: req.body.mainCategory, subCategory: req.body.subCategory, fit: req.body.fit} },
+    const updatedApparel = await Wardrobe.findByIdAndUpdate(
+      { _id: req.params.apparelId},
+      {
+        mainCategory: req.body.mainCategory,
+        subCategory: req.body.subCategory,
+        fit: req.body.fit
+      },
       {new: true}
-      );
+    );
     debug("found apparel by user: %o", updatedApparel);
     res.status(200).json({
       status: "success",
       data: {
-        apparel,
+        apparel: updatedApparel,
       },
     });
   } catch (err) {
@@ -107,4 +112,4 @@ async function updateOne(req, res) {
   }
 }
 
-module.exports = { uploadImg, create, getAll, del, updateOne};
+module.exports = { uploadImg, create, getAll, del, updateOne };

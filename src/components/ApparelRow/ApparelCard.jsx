4cfe0dfbf4de29cb6f2dab5766182a148efdcaf1
ryu <cristellechoo@hotmail.com>
2023-10-09 11:00:52 +0800

    feat: add route for ApparelEditForm

--- src/components/ApparelRow/ApparelCard.jsx
+++ src/components/ApparelRow/ApparelCard.jsx
@@ -1,12 +1,20 @@
 import { RxCross1 } from "react-icons/rx";
 import { PiPencil } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
 
 function ApparelCard({ item, handleDelete }) {
+  const navigate = useNavigate();
+  const navEdit = function() {
+    navigate('/wardrobe/:apparelId/edit')
+  }
   return (
     <article>
       <div className="bg-stone-400 p-3 m-1 rounded-lg shadow md:flex-row md:max-w-xl">
         <span className="flex items-center justify-end">
-          <PiPencil className="text-md mb-2 mr-1 fill-black" />
+          <PiPencil 
+          onClick={navEdit}
+          className="text-md mb-2 text-black cursor-pointer"
+          />
           <RxCross1
             onClick={() => handleDelete(item._id)}
             className="text-md mb-2 text-black cursor-pointer"

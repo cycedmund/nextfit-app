79d93e7877c6bc21c6132202327eca5feb655a3a
cycedmund <cycedmund@gmail.com>
2023-10-10 03:26:14 +0800

    feat: add handleUpdateWornFreq to frontend

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,32 @@
 import Weather from "../../components/Weather/Weather";
+import Swal from "sweetalert2";
+import {
+  patchApparelFrequencyService,
+  swalBasicSettings,
+} from "../../utilities/wardrobe-service";
 
 function HomePage() {
+  const handleUpdateWornFreq = async (outfitIDs) => {
+    try {
+      await patchApparelFrequencyService(outfitIDs);
+      Swal.fire({
+        ...swalBasicSettings("Updated!", "success"),
+        text: "Thank you for reusing your clothes!",
+      });
+    } catch (err) {
+      console.error(err);
+      Swal.fire({
+        ...swalBasicSettings("Error", "error"),
+        text: "Something went wrong",
+      });
+    }
+  };
   return (
     <>
-    <h1 className="ml-24 mt-6 text-2xl">My Wardrobe</h1>
-    <Weather />
+      <h1 className="ml-24 mt-6 text-2xl">My Wardrobe</h1>
+      <Weather handleUpdateWornFreq={handleUpdateWornFreq} />
     </>
-    )
+  );
 }
 
 export default HomePage;

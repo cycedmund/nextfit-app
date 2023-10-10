bc34db5ea6cd98494606f287be0b31ae96f184bb
cycedmund <cycedmund@gmail.com>
2023-10-10 20:51:04 +0800

    edit Apparel Card and NavBar

---
+++ src/pages/WardrobePage/WardrobeHero.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function WardrobeHero() {
+  return (
+    <div className="hero h-[80vh] bg-black">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold mb-8">
+            Start by adding to your wardrobe!
+          </h1>
+          <Link to="/wardrobe/new">
+            <button className="btn btn-ghost text-white text-2xl bg-[#E50A14] hover:bg-[#c11119] rounded-md normal-case">
+              Start Your Journey
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WardrobeHero;
